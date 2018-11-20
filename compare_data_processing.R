02716c3f1862eff96251218fc17206b27215197b
Candace Savonen <savonenc@msu.edu>
2018-11-20 12:21:38 -0500

    PCA of Salmon and HISAT data with original data

--- compare_data_processing.R
+++ compare_data_processing.R
@@ -61,34 +61,64 @@ gsm.ids <- dplyr::recode(sra.srx$experiment[match(
 colnames(hisat.data)[-1] <- as.character(gsm.ids)
 
 #------------------------Compare hisat and author data-------------------------#
-# Correlate hisat and author processed data
-sample.ind <- match(colnames(hisat.data)[-c(1:2)],
-                    colnames(author.data))
-gene.ind <- match(hisat.data$gene,
-                  rownames(author.data))
+
+compareData <- function (data = data, org.data = author.data, label = "label") {
+
+# Get genes 
+genes <- data$gene
+
+# Only look at the data columns that are samples
+data <- data[, grep("GSM", colnames(data))]
+
+# Get indices of matching samples and genes
+sample.ind <- match(colnames(data),
+                    colnames(org.data))
+gene.ind <- match(genes,
+                  rownames(org.data))
+
+# Combine the data into one dataset
+combined <- cbind(data[!is.na(gene.ind),], 
+org.data[gene.ind[!is.na(gene.ind)], sample.ind])
+
+process <- as.factor(c(rep(label, ncol(data)), rep("original", ncol(org.data))))
+pca <- prcomp(t(combined))
+
+# This function will plot the distribution of the PC scores looks like and label samples by the different variables' different factor levels
+pc.plot <- function(dat,var){
+  colz <- colors(distinct=TRUE)[runif(length(levels(var)),min=1,max=length(colors(distinct=TRUE)))]
+  plot(dat,pch=21,bg=colz[var]);
+  legend(x="topleft", legend = levels(var),fill=colz,cex=.8)
+}
+
+# Print out a PCA plot
+jpeg(file.path("..", "results", paste0("PCA", label, ".jpeg")))
+pc.plot(pca$x, process)
+dev.off()
 
 Rsqs <- c()
+  for (sample in 1:length(sample.ind)) {
+    # Correlate the two datasets
+    Rsq <- cor(data[!is.na(gene.ind),][,sample],
+               
+               org.data[gene.ind[!is.na(gene.ind)], sample.ind][,sample])
 
-for (i in 1:length(sample.ind)) {
-# Correlate the two datasets
-Rsq <- cor(hisat.data[!is.na(gene.ind),-c(1:2)][,i], author.data[gene.ind[!is.na(gene.ind)], sample.ind][,i])
+    # Keep all the Rsq's
+    Rsqs <- c(Rsqs, Rsq)
 
-# Keep all the Rsq's
-Rsqs <- c(Rsqs, Rsq)
-
-# Save scatterplot to jpeg
-jpeg(paste0("../results/Sample", i , ".jpeg"))
-plot(hisat.data[!is.na(gene.ind),-c(1:2)][,i],
-     author.data[gene.ind[!is.na(gene.ind)], sample.ind][,i],
+    # Save scatterplot to jpeg
+    jpeg(file.path("..", "results", paste0("Sample", sample, label, ".jpeg")))
+    plot(data[!is.na(gene.ind),][,sample],
+     org.data[gene.ind[!is.na(gene.ind)], sample.ind][,sample],
      xlab = paste0("HISAT"),
      ylab = paste0("Original"),
      main = paste0("R = ", round(Rsq, 3)))
-dev.off()
+    dev.off()
+    }
+
 }
+# Run this with the hisat data
+compareData(hisat.data, author.data, "HISAT")
 
-# Correlate salmon and author processed data
-sample.ind <- match(dimnames(salmon.data)[[2]], dimnames(author.data)[[2]])
-corr(salmon.data, author.data[gene.id, sample.ind])
-
-
+# Run this with the salmon data
+compareData(salmon.data, author.data, "Salmon")
 
