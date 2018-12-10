73cbad183b59cde2c7375e72823b48fd6662220c
Candace Savonen <savonenc@msu.edu>
2018-12-10 09:11:23 -0500

    Documentation for get_sample_download list.

--- 0-get_sample_download_list.R
+++ 0-get_sample_download_list.R
@@ -1,7 +1,25 @@
 # CCDL ALSF 2018
 # C. Savonen 
 # 
-# Get raw reads for the given SRP Id
+# Get list of all the samples for the given SRP Id. Optionally, refresh a sample 
+# list to not include already downloaded files. Makes "files.to.download.txt" file 
+# which run_pipeline.sh files use for getting the sample IDs to download. 
+
+# Options: 
+# "-i" - the project ID which you wish to download the samples for. eg "SRP079058"
+# "-d" - the directory which you would like the downloaded samples to go to.
+#        This directory will be created if it doesn't exist, but if it does 
+#        exist and the refresh option is used, then this directory will be 
+#        evaluated for what samples have already been downloaded. 
+# "-r" - If used, the directory given will be evaluated for what samples have 
+#        been downloaded and those samples will not be added to the list for
+#        re-download. 
+# "-n" - This option is for extracting a random subset of the sample set. If
+#        you want to do a test run, put how many samples you would like to be
+#        randomly selected. "-n 20" for downloading 20 samples for example. 
+# "-q" - put the path to the GEO metadata sql, if the file already exists. 
+#        The sql file will be downloaded if this option isn't given. 
+
 # Magrittr pipe
 `%>%` <- dplyr::`%>%`
 
@@ -18,9 +36,10 @@ option_list <- list(
               help = "Directory where you would like the data downloaded to go",
               metavar = "character"),
   make_option(c("-r", "--refresh"), action ="store_true", default = FALSE,
-              help="Use this option if you don't want to re-process already existing files"),
+              help = "Use this option if you don't want to re-process already
+              existing files"),
   make_option(c("-n", "--number"), type = "numeric", default = NULL,
-              help="If you'd like to only use a portion of the samples,
+              help = "If you'd like to only use a portion of the samples,
               state the max number you'd like to download."),
   make_option(opt_str = c("-q", "--sql"), type = "character", default = NULL,
               help = "Directory path to sql", metavar = "character")
@@ -30,11 +49,8 @@ opt <- parse_args(OptionParser(option_list = option_list))
 
 # Check if the sql file from GEO is where the option says.
 if (!is.null(opt$sql)) {
-  if (!file.exists(opt$sql)) {
-      stop("The path to the GEO sql file does not exist. Double check that the file
-       is at the path you specified relative to the current directory.")
+      stop("The path to the GEO sql file has not been specified.")
   }
-}
 #------------------- Connect to NCBI's SRA SQL database------------------------#
 # Get sra path
 srafile <- file.path(opt$sql)
