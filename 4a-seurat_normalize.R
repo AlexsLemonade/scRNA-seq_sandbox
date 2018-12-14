33decf907995d5cd50aa0593464a4f01d33a5f17
Candace Savonen <savonenc@msu.edu>
2018-12-14 11:18:55 -0500

    Cosmetic and documentation changes

--- 4a-seurat_normalize.R
+++ 4a-seurat_normalize.R
@@ -1,11 +1,13 @@
 # CCDL for ALSF 2018
 # C. Savonen
 #
-# Purpose: Using Seurat for post-processing of scRNA-seq Salmon processed data
+# Purpose: Using Seurat for post-processing of scRNA-seq gene matrix .tsv 
+#          In gene x sample format. 
 #
 # Options:
 # "-d" - Directory of where individual samples' salmon folders are located.
-# "-o" - Directory of where the output gene matrix RDS file should go.(Optional)
+# "-o" - Directory of where the output gene matrix RDS file should go. Will go 
+#        to current directory if none is specified. (Optional)
 # "-l" - Optional label to add to output files. Generally necessary if processing
 #        multiple datasets in the same pipeline.
 # 
@@ -55,10 +57,10 @@ tx.counts <- readr::read_delim(opt$data)
 # This list is from here :http://jdblischak.github.io/singleCellSeq/analysis/qc-filter-ipsc.html
 # Identify mitochondrial genes by their ensembl IDs
 mtgene <- c("ENSG00000198899", "ENSG00000198727", "ENSG00000198888",
-"ENSG00000198886", "ENSG00000212907", "ENSG00000198786",
-"ENSG00000198695", "ENSG00000198712", "ENSG00000198804",
-"ENSG00000198763","ENSG00000228253", "ENSG00000198938",
-"ENSG00000198840")
+            "ENSG00000198886", "ENSG00000212907", "ENSG00000198786",
+            "ENSG00000198695", "ENSG00000198712", "ENSG00000198804",
+            "ENSG00000198763","ENSG00000228253", "ENSG00000198938",
+            "ENSG00000198840")
 
 # Get the gene symbol names for these genes
 mtgene <- tx.counts$gene[match(mtgene, tx.counts$ensembl)]
@@ -68,15 +70,15 @@ mtgene <- paste0("^", mtgene, "$")
 
 # Set up data in Seurat format
 seurat <- CreateSeuratObject(raw.data = tx.counts, min.cells = 3,
-                             min.genes = 200, project = "darmanis_gbm")
+                             min.genes = 200, project = opt$label)
 
-# Get those mito genes
+# Get those mitochondrial genes
 mito.genes <- grep(pattern = paste0(mtgene, collapse = "|"),
                    x = rownames(seurat@data), value = TRUE)
 
-# Calculate the percentage of mitochonrial transcripts to total transcripts
-percent.mito <- Matrix::colSums(seurat@raw.data[mito.genes, ])/
-Matrix::colSums(seurat@raw.data)
+# Calculate the percentage of mitochondrial transcripts to total transcripts
+percent.mito <- 
+    Matrix::colSums(seurat@raw.data[mito.genes, ])/Matrix::colSums(seurat@raw.data)
 
 # AddMetaData adds columns to object@meta.data, and is a great place to
 # stash QC stats
@@ -84,14 +86,16 @@ seurat <- AddMetaData(object = seurat, metadata = percent.mito,
                       col.name = "percent_mito")
 
 # Filter out cells
-seurat <- FilterCells(object = seurat, subset.names = c("nGene", "percent_mito"),
-                      low.thresholds = c(200, -Inf),
-                      high.thresholds = c(2500, 0.05))
+seurat.filtered <- FilterCells(object = seurat, 
+                               subset.names = c("nGene", "percent_mito"),
+                               low.thresholds = c(200, -Inf),
+                               high.thresholds = c(2500, 0.05))
 
 # Normalizing the data
-seurat <- NormalizeData(object = seurat, normalization.method = "LogNormalize",
-                        scale.factor = 10000)
+seurat.norm <- NormalizeData(object = seurat.filtered, 
+                             normalization.method = "LogNormalize",
+                             scale.factor = 10000)
 
-# Save this seurat object to an RDS file
-readr::write_tsv(seurat, file = file.path(opt$output,
-                                 paste0(opt$label, "seurat_normalize.tsv"))
+# Save this seurat object to an tsv file
+out.file <- file.path(opt$output, paste0(opt$label, "seurat_normalize.tsv"))
+readr::write_tsv(seurat.norm, file = out.file)
