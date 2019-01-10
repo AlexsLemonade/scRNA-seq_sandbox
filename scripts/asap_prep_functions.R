1946c2b45916988c3f76bb59a029cd130e6ce158
Candace Savonen <savonenc@msu.edu>
2019-01-10 10:21:16 -0500

    Fixed gene column problem

--- scripts/asap_prep_functions.R
+++ scripts/asap_prep_functions.R
@@ -5,11 +5,11 @@
 # dataset that is ready for uploading to [ASAP online](https://asap.epfl.ch/)
 
 AsapFilter <- function(data, genes, min_counts = 1, perc_genes = 0.01, num_genes = 100) {
-  # This function is filters and makes data into ASAP format
+  # This function is filters and makes data into ASAP format and assumes gene info 
+  # is the first column
   # Args:
-  #  data: a gene expression data.frame that is gene x samples
-  #  genes: a character vector containing the gene names (corresponding to the
-  #         rows in data)
+  #  data: a gene expression data.frame that is gene x samples, with the first column
+  #        containing the gene names
   #  min_counts: the cutoff for minimum number of counts for a gene to be 
   #              considered expressed in a particular sample
   #  perc_genes: The minimum percent of samples that need to express that gene 
@@ -19,25 +19,29 @@ AsapFilter <- function(data, genes, min_counts = 1, perc_genes = 0.01, num_genes
   # Returns:
   #   A filtered gene expression data.frame, that is able to be submitted to ASAP
   # Get rid of decimal points
+  # 
+  # Store the genes separately
+  gene <- data[, 1]
+  
+  # Get rid of decimals (even if they are .000, ASAP doesn't like them)
   data <- apply(data[,-1], 2, round)
-  rownames(data) <- genes
 
   # Identify which cells have at least 1 count
-  boolean <- data >= min_counts
+  detected <- data >= min_counts
 
   # Filter genes that are expressed in 1% of cells
-  gene.sum <- apply(boolean, 1, sum)
+  gene.sum <- apply(detected, 1, sum)
   data <- data[which(gene.sum > ncol(data)*perc_genes), ]
 
   # Identify which cells have at least 1 count
-  boolean <- data >= min_counts
+  detected  <- data >= min_counts
 
   # Filter samples that express at least 100 genes
-  sample.sum <- apply(boolean, 2, sum)
+  sample.sum <- apply(detected, 2, sum)
   data <- data[, which(sample.sum > num_genes) ]
 
   # Need the genes to be its own column
-  data <- data.frame("gene" = rownames(data), data)
+  data <- data.frame(gene, data)
   
   return(data)
 }
