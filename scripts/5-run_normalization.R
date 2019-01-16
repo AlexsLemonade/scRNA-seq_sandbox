0966af30d982e115caf3df1b63f4558b17897b40
Candace Savonen <savonenc@msu.edu>
2019-01-16 11:47:04 -0500

    gene column error

--- scripts/5-run_normalization.R
+++ scripts/5-run_normalization.R
@@ -4,11 +4,12 @@
 # Purpose: Normalizing count data from single rna-seq
 #
 # Adapted from Automated Single-cell Analysis Pipeline (ASAP) github url:
-# https://github.com/DeplanckeLab/ASAP/blob/master/R_Python/normalization.R
+# https://github.com/DeplanckeLab/ASAP/blob/88ca3716d09197f7d178761015ea4f6a2783dd5e/R_Python/normalization.R
 #
 # Options:
 # '-d' : Path to gene matrix in tab delimited format, gene x sample with gene
-#        info as the first column
+#        info as the first column. With gene info column labeled as 'gene' or 
+#        'genes' (not case sensitive)",
 # '-a' : Normalization method to use. To run all methods use "all", to run more
 #        than one method, separate the names by a space. eg -a tmm log
 #        Options:
@@ -20,7 +21,8 @@
 #           'tmm'- trimmed mean M values by edgeR
 #           'all'- run all the above methods
 # '-o' : Directory where you would like the output to go. Default is current
-#        directory
+#        directory. New folder will be created if the specified folder doesn't 
+#        exist.
 # '-l' : Optional label for output files"
 #
 # Command line example:
@@ -39,11 +41,13 @@ library(optparse)
 option_list <- list(
   make_option(opt_str = c("-d", "--data"), type = "character", default = "none",
               help = "Path to gene matrix in tab delimited format, gene x sample
-              with gene info as the first column",
+              with gene info column labeled as 'gene' or 'genes' (not case sensitive)",
               metavar = "character"),
   make_option(opt_str = c("-a", "--algorithm"), type = "character",
               default = "none", help = "Normalization method to use. Options:
-              'scale', log', 'voom', 'deseq2', 'vsd', 'rld' or 'tmm' ",
+              'scale', log', 'voom', 'deseq2', 'vsd', , 'tmm' or use 'all'
+              to run all algorithms. If multiple methods are wanted, separate 
+              with a space eg: 'log voom'",
               metavar = "character"),
   make_option(opt_str = c("-o", "--output"), type = "character",
               default = getwd(), help = "Directory where you would like the
@@ -59,7 +63,7 @@ opt <- parse_args(OptionParser(option_list = option_list))
 # Stop if no input data matrix is specified
 if (opt$data == "none") {
     stop("Error: no specified input gene matrix file. Use option -d to specify
-    the gene matrix tsv data you would like to normalize")
+         the gene matrix tsv data you would like to normalize")
 }
 
 # Create the output for results folder if it does not exist
@@ -68,30 +72,47 @@ if (!dir.exists(opt$output)) {
     dir.create(opt$output)
 }
 
-#---------------------------Set up algorithms to run---------------------------#
-# Check that the dimension reduction option given is supported
-all.algorithms <- c( 'scale', 'log', 'voom','tmm', 'deseq2', 'vsd', 'rld')
+# Find which column has gene info
+gene.col <- grep("gene", colnames(dataset), ignore.case = TRUE)
 
-# If "all" is chosen, run all the normalization methods
+# Create the output for results folder if it does not exist
+if (length(gene.col) < 1) {
+  stop("Error: Cannot find a column with gene info. (Looking for 'gene' as 
+        column name")
+}
+
+# Append an underscore to the label if it is given
+if (opt$label != "") {
+  opt$label <- paste0("_", opt$label)
+}
+
+#---------------------------Set up algorithms to run---------------------------#
+# List all the supported algorithms
+all.algorithms <- c('scale', 'log', 'voom','tmm', 'deseq2', 'vsd')
+
+# If "all" is chosen, put all the normalization methods in list
 if (opt$algorithm == "all") {
     opt$algorithm <- all.algorithms
 } else {
+    # Separate if multiple algorithms have been requested
     opt$algorithm <- unlist(strsplit(opt$algorithm, " "))
 }
 
 # Check that the algorithms chosen are in the supported list
 if (any(is.na(match(opt$algorithm, all.algorithms)))) {
-    stop("That is not a normalization method supported by this script.
-    Check for typos. Acceptable options:'scale', 'log', 'voom','tmm',
-    'deseq2', 'vsd', 'rld'")
+    stop("No supported normalization method was given. Check for typos.
+         Use option -a to choose and algorithm. Acceptable options:'scale', 'log',
+         'voom','tmm','deseq2', 'vsd',  or 'all")
 }
 #----------------------------------Load data-----------------------------------#
 # Read in a tsv file of data
 dataset <- readr::read_tsv(opt$data)
 
 # Separate genes from the numeric data
-genes <- dataset$gene
-dataset <- dataset[, -1]
+genes <- dataset[, gene.col]
+
+# Make dataset the data only
+dataset <- dataset[, -gene.col]
 
 #-----------------------Run each algorithm in the list-------------------------#
 # For each algorithm, run through this loop
@@ -111,21 +132,21 @@ for (algorithm in opt$algorithm) {
         
     } else  if (algorithm == "voom") {
         data.out <- as.data.frame(limma::voom(counts = dataset,
-        normalize.method = "quantile",
-        plot = FALSE)$E)
+                                              normalize.method = "quantile",
+                                              plot = FALSE)$E)
         title <- "Log2 Expression (Voom)"
         
     } else if (algorithm == "tmm") {
         data.dge <- edgeR::DGEList(counts = dataset)
         data.dge <- edgeR::calcNormFactors(data.dge)
         data.out <- as.data.frame(edgeR::cpm(data.dge, normalized.lib.sizes = TRUE,
-        log = TRUE))
+                                             log = TRUE))
         title <- "Log2 Expression (TMM / edgeR)"
         
     } else if (algorithm == "deseq") {
         data.colData <- data.frame(row.names = colnames(dataset))
         data.dds <- DESeq2::DESeqDataSetFromMatrix(dataset, colData = data.colData,
-        design = ~1)
+                                                   design = ~1)
         data.dds <- DESeq2::estimateSizeFactors(data.dds)
         data.out <- as.data.frame(DESeq2::counts(data.dds, normalized = TRUE))
         data.out <- sign(data.out) * log2(1 + abs(data.out))
@@ -134,10 +155,10 @@ for (algorithm in opt$algorithm) {
     } else if (algorithm == "vsd") {
         data.colData <- data.frame(row.names = colnames(dataset))
         data.dds <- DESeq2::DESeqDataSetFromMatrix(dataset, colData = data.colData,
-        design = ~1)
+                                                   design = ~1)
         data.out <- SummarizedExperiment::assay(
-        DESeq2::varianceStabilizingTransformation(data.dds,
-        blind = TRUE))
+                    DESeq2::varianceStabilizingTransformation(data.dds, 
+                                                              blind = TRUE))
         title <- "Log2 Expression (varianceStabilizingTransformation - DEseq2)"
         
     }
@@ -145,14 +166,14 @@ for (algorithm in opt$algorithm) {
     perc.zero <- round(length(which(data.out == 0)) / length(as.matrix(data.out)), 3)
     
     # Build output file name:
-    output.file <- paste0(opt$output, "/", algorithm, "_", opt$label, ".tsv")
+    output.file <- file.path(opt$output, paste0(algorithm, opt$label, ".tsv"))
     
     # Print out summary:
     cat("\n normalization method:", title,
-    "\n number of genes:", nrow(data.out),
-    "\n number of cells:", ncol(data.out),
-    "\n percent zeroes:", perc.zero,
-    "\n\n results file:", output.file)
+        "\n number of genes:", nrow(data.out),
+        "\n number of cells:", ncol(data.out),
+        "\n percent zeroes:", perc.zero,
+        "\n\n results file:", output.file)
     
     # Save normalized data to a tsv file
     data.out <- data.frame("genes" = genes, data.out)
